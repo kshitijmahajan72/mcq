509ca92833423d8666d5c18217ec631017b0606d
kshitijmahajan72 <sajan.mahajan72@gmail.com>
2018-05-25 23:08:47 +0530

    fixing issues of the linking

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -11,7 +11,7 @@ export class TeacherComponent implements OnInit {
   constructor() {
   }
   ngOnInit() {
-    window.location.href = 'https://docs.google.com/forms/d/1bpSrJdB5ZgqvmPUvM5BlwBGxP6R-VUHGzHdktgil7Lg/edit';
+    window.location.href = 'https://docs.google.com/forms/d/1v6fhdwqo_0E7mi8U2xdjdH_8IYGMuCZtCQ9rHFZrIZ8/edit';
   }
 
 }
